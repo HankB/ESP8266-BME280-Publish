bc26383c5858177e70eca7decfaac855bd8eb595
Hank Barta <hbarta@gmail.com>
2021-12-08 20:57:54 -0600

    added NTP client

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <NTPClient.h>   //https://github.com/arduino-libraries/NTPClient
+#include <WiFiUdp.h>
 
 /* secrets.h should define the following as appropriate for your network.
 const char* ssid = "...";
 const char* password = "...";
 const char* mqtt_server = "host.domain"; // "<host>.localddomain" works for me.
+const char* ntp_server = "host.domain"; // "<host>.localddomain" works for me.
 */
 #include "secrets.h"
 
 #define serial_IO true
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, ntp_server);
 
 void setup_wifi(void)
 {
@@ -52,9 +58,20 @@ void setup()
 #endif
 
   setup_wifi();
+  timeClient.begin();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  unsigned long before = micros();
+  timeClient.update();
+  unsigned long epoch = timeClient.getEpochTime();
+  unsigned long after = micros();
+
+
+  Serial.print(epoch);
+  Serial.print(" ");
+  Serial.println(after-before);
+
+  delay(1000);
 }
